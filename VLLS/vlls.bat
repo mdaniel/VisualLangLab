e3a8701dc747ac86eccd1f1139f5794d1edc0a31
sanjay_dasgupta <sanjay_dasgupta@c0272bae-d8de-c28f-bcdf-8b42aff4940e>
2011-10-25 11:19:55 +0000

    Updated launcher scripts for VLL-48
    git-svn-id: https://svn.java.net/svn/vll~svn/trunk@213 c0272bae-d8de-c28f-bcdf-8b42aff4940e

--- VLLS/vlls.bat
+++ VLLS/vlls.bat
@@ -1,1 +1,7 @@
-scala -classpath VLLS.jar -D"java.util.Arrays.useLegacyMergeSort=true" vll.gui.VllGui
+@ECHO OFF
+IF "%SCALA_HOME%"=="" (
+  ECHO "'SCALA_HOME' IS NOT DEFINED, EXITING"
+) ELSE (
+  ECHO "Launching VisualLangLab ..."
+  java -D"java.util.Arrays.useLegacyMergeSort=true" -cp ".;VLLS.jar;%SCALA_HOME%\lib\scala-library.jar;%SCALA_HOME%\lib\scala-compiler.jar;%SCALA_HOME%\lib\scala-swing.jar" vll.gui.VllGui
+)
