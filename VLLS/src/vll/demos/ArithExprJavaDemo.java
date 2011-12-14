db3bf1023fc266de5ab2f6e4a11b85cb74b8eda4
sanjay_dasgupta <sanjay_dasgupta@c0272bae-d8de-c28f-bcdf-8b42aff4940e>
2011-12-14 04:55:40 +0000

    Updates for binary release version 7.02.
    git-svn-id: https://svn.java.net/svn/vll~svn/trunk@241 c0272bae-d8de-c28f-bcdf-8b42aff4940e

--- VLLS/src/vll/demos/ArithExprJavaDemo.java
+++ VLLS/src/vll/demos/ArithExprJavaDemo.java
@@ -1,23 +1,22 @@
 /*
-    Copyright 2010, Sanjay Dasgupta
-    sanjay.dasgupta@gmail.com
+Copyright 2010, Sanjay Dasgupta
+sanjay.dasgupta@gmail.com
 
-    This file is part of VisualLangLab (http://vll.java.net/).
+This file is part of VisualLangLab (http://vll.java.net/).
 
-    VisualLangLab is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+VisualLangLab is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    VisualLangLab is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+VisualLangLab is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with VisualLangLab.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
+You should have received a copy of the GNU General Public License
+along with VisualLangLab.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package vll.demos;
 
 import java.io.File;
@@ -25,53 +24,44 @@ import java.util.List;
 import vll.core.VllParsers;
 
 public class ArithExprJavaDemo {
-    
-  private static Integer ZERO = new Integer(0);
-  private static Integer ONE = new Integer(1);
-    
-  static Float evalFactorAST(Object ast) {
-      Object[] pair = (Object[]) ast;
-      Float factorResult = -1f;
-      if (pair[0].equals(ZERO)) {
-          factorResult =  Float.parseFloat((String)pair[1]);
-      } else if (pair[0].equals(ONE)) {
-          factorResult = evalExprAST(pair[1]);
-      }
-      return factorResult;
-  }
-  
+
+    static Float evalFactorAST(Object ast) {
+        Object[] pair = (Object[]) ast;
+        Float factorResult = -1f;
+        if (pair[0].equals(0)) {
+            factorResult = Float.parseFloat((String) pair[1]);
+        } else if (pair[0].equals(1)) {
+            factorResult = evalExprAST(pair[1]);
+        }
+        return factorResult;
+    }
+
     static Float evalExprAST(Object ast) {
         Object[] arr = (Object[]) ast;
         Float exprResult = evalTermAST(arr[0]);
-//        Iterator<Object> theTerms = (Iterator<Object>) arr[1];
-//        while (theTerms.hasNext()) {
-//          Object[] pair = (Object[]) theTerms.next();
-        for (Object[] pair: (List<Object[]>) arr[1]) {
-          if (pair[0].equals(ZERO)) {
-              exprResult += evalTermAST(pair[1]);
-          } else if (pair[0].equals(ONE)) {
-              exprResult -= evalTermAST(pair[1]);
-          }
+        for (Object[] pair : (List<Object[]>) arr[1]) {
+            if (pair[0].equals(0)) {
+                exprResult += evalTermAST(pair[1]);
+            } else if (pair[0].equals(1)) {
+                exprResult -= evalTermAST(pair[1]);
+            }
         }
-      return exprResult;
-  }
-  
-  static Float evalTermAST(Object ast) {
+        return exprResult;
+    }
+
+    static Float evalTermAST(Object ast) {
         Object[] arr = (Object[]) ast;
         Float termResult = evalFactorAST(arr[0]);
-//        Iterator<Object> theFactors = (Iterator<Object>) arr[1];
-//        while (theFactors.hasNext()) {
-//          Object[] pair = (Object[]) theFactors.next();
-        for (Object[] pair: (List<Object[]>) arr[1]) {
-          if (pair[0].equals(ZERO)) {
-              termResult *= evalFactorAST(pair[1]);
-          } else if (pair[0].equals(ONE)) {
-              termResult /= evalFactorAST(pair[1]);
-          }
+        for (Object[] pair : (List<Object[]>) arr[1]) {
+            if (pair[0].equals(0)) {
+                termResult *= evalFactorAST(pair[1]);
+            } else if (pair[0].equals(1)) {
+                termResult /= evalFactorAST(pair[1]);
+            }
         }
-      return termResult;
-  }
-  
+        return termResult;
+    }
+
     public static void main(String[] args) {
         String input = "(3 + 5) * (8 - 4)";
         VllParsers vll = VllParsers.fromFile(new File("ArithExpr.vll"));
@@ -85,5 +75,4 @@ public class ArithExprJavaDemo {
             System.out.println(parseResult);
         }
     }
-    
 }
