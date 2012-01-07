79a3954dffc5b6250cbbf114560cac63aa33664d
sanjay_dasgupta <sanjay_dasgupta@c0272bae-d8de-c28f-bcdf-8b42aff4940e>
2012-01-07 12:16:31 +0000

    Binary release version change to 10.03.
    git-svn-id: https://svn.java.net/svn/vll~svn/trunk@281 c0272bae-d8de-c28f-bcdf-8b42aff4940e

--- VLL4J/src/vll4j/gui/Vll4jGui.java
+++ VLL4J/src/vll4j/gui/Vll4jGui.java
@@ -259,5 +259,5 @@ public class Vll4jGui extends JFrame {
     int frameWidth = (int) (screenSize.width * 0.75);
     int frameHeight = (int) (screenSize.height * 0.75);
     private String titleString;
-    String version = "10.02";
+    String version = "10.03";
 }
