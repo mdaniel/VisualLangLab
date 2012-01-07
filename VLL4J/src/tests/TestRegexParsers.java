fd59cd82284eb2724fb740d6a114b2619c929899
sanjay_dasgupta <sanjay_dasgupta@c0272bae-d8de-c28f-bcdf-8b42aff4940e>
2012-01-07 12:14:24 +0000

    Changes due to corrections in RegexParsers and SimpleLexingRegexParsers.
    git-svn-id: https://svn.java.net/svn/vll~svn/trunk@280 c0272bae-d8de-c28f-bcdf-8b42aff4940e

--- VLL4J/src/tests/TestRegexParsers.java
+++ VLL4J/src/tests/TestRegexParsers.java
@@ -1,6 +1,7 @@
 package tests;
 
 import java.util.List;
+import java.util.regex.Pattern;
 import vll4j.core.Parsers.ParseResult;
 import vll4j.core.Parsers.Parser;
 import vll4j.core.RegexParsers;
@@ -9,8 +10,8 @@ public class TestRegexParsers {
     public static void main(String args[]) {
 //        System.out.println(System.getProperty("java.home"));
         RegexParsers me = new RegexParsers();
-        Parser<String> nbr = me.regex("\\d+");
-        Parser<String> name = me.regex("[a-z]+");
+        Parser<String> nbr = me.regex(Pattern.compile("\\d+"));
+        Parser<String> name = me.regex(Pattern.compile("[a-z]+"));
         Parser<String> semi = me.literal(";");
         Parser<Object[]> line = me.sequence(0, name, nbr, semi);
         ParseResult pr = me.parseAll(line, "sanjay \t\r\n  100   ;  ");
